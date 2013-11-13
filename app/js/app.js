574ec9071b3e3afc49d6792a77404a8cff6b3e3f
Coalman <tehcoalman@gmail.com>
2013-11-12 22:37:25 -0500

    Made it possible for a card's contents to be inserted into the dom as html instead of an interpolated string.

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ui.router']);
+var app = angular.module('app', ['ui.router', 'ngSanitize']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 	$stateProvider

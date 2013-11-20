3c2d72885e8cf174c58408cf661f3666ce25db60
Coalman <tehcoalman@gmail.com>
2013-11-19 21:09:33 -0500

    Started implementing import buttons.

---
+++ app/js/controllers/import_controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+app.controller('ImportController', function($scope, DeckService, DeckParser) {
+	$scope.importGithub = function() {
+
+	};
+
+	$scope.importLocal = function() {
+
+	};
+});

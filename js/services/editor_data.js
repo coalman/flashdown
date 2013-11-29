4eb8423f34b40d0a5b0ed30b5a98e50d6127a227
Coalman <tehcoalman@gmail.com>
2013-11-29 02:05:11 -0500

    Setting initial raw string.

--- js/services/editor_data.js
+++ js/services/editor_data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app.service('EditorData', function(DeckData) {
-	this.rawText = '';
+	this.rawText = '* front\n- back';
 
 	this.parseText = function() {
 		DeckData.parse(this.rawText);
